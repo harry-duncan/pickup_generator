e5f6532a43bc3dc2cfa0e6fc366d71a955490391
Harry Duncan <harryduncan25@gmail.com>
2016-03-20 16:05:00 +1100

    pull commit

--- js/main.js
+++ js/main.js
@@ -4,11 +4,11 @@ $(document).ready(function(){
 
   $('.main').on('submit', '.form', function (e) {
     e.preventDefault();
-    $.ajax("http://api.giphy.com/v1/gifs/search?q=" + noun +  "+" + adjective + "&api_key=dc6zaTOxFJmzC", {
+    $.ajax("http://api.giphy.com/v1/gifs/search?q=" + noun + "+" + adjective + "&api_key=dc6zaTOxFJmzC", {
       type:'GET'
     }).done(function(){
       console.log("get request successful");
-      $('.gif').append("<img >");
+      $('.gif').append("<img>");
     })
   });
 
